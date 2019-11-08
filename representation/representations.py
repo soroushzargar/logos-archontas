ac981a1b3a01ff4ce69b76291892cc43d039b5cd
soroushzargar <soroushzargar@gmail.com>
2019-11-08 19:51:52 +0330

    added wav representation and correction in spectrogram

--- representation/representations.py
+++ representation/representations.py
@@ -11,19 +11,25 @@ import numpy as np
 
 class representation:
     @staticmethod
-    def spectrogram(audioSignal, audioSr, mod="RE"):
-        return librosa.feature.melspectrogram(
-            y=audioSignal,
-            sr=audioSr
-        )
+    def spectrogram(audioSignal, audioSr, mode="mel"):
+        if mode == "mel":
+            return librosa.feature.melspectrogram(
+                y=audioSignal,
+                sr=audioSr
+            )
+        if mode == "RE":
+            return librosa.core.stft(
+                y=audioSignal
+            )
+        return None
 
     @staticmethod
     def spectrogramFigure(audioSignal=None, audioSr=16000,
                           audioSpectrogram=None, show=True,
-                          save=False, saveLocation=None):
+                          save=False, saveLocation=None, mode="mel"):
         if audioSignal is not None:
             audioSpectrogram = representation.spectrogram(
-                audioSignal, audioSr
+                audioSignal, audioSr, mode=mode
             )
 
         if audioSpectrogram is not None:
@@ -36,6 +42,16 @@ class representation:
             plt.title('Mel-frequency spectrogram')
             plt.tight_layout()
 
+        if save is True and saveLocation is not None:
+            plt.savefig(saveLocation)
+        if show is True:
+            plt.show()
+
+    def waveFigure(audioSignal=None, audioSr=16000,
+                   audioSpectrogram=None, show=True,
+                   save=False, saveLocation=None, mode="mel"):
+        librosa.display.waveplot(y, sr=sr)
+        plt.title('Stereo')
         if save is True and saveLocation is not None:
             plt.savefig(saveLocation)
         if show is True:
